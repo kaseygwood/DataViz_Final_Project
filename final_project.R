a6b2b9d066d178d244830d167e7f8ee378bb53a0
kaseygwood <kgwood20@stlawu.edu>
2022-03-28 08:53:46 -0400

    March 28 graded before class commit. Added inputs and one output of the top three in the event.

--- final_project.R
+++ final_project.R
@@ -7,8 +7,11 @@ library(shiny)
 ui <- fluidPage(
   sidebarLayout(selectizeInput("Competition",
                                label = "Choose the Competition",
-                               choices = detailed_results$series),
-  mainPanel(navlistPanel(
+                               choices = c("Champions Series", "Championships (25m)", "Championships(50m)", "Junior Championships", "Marathon Series", "Olympic Games", "World Cup", "Youth Olympic Games")),
+                radioButtons("Style",
+                             label = "Choose the Stroke",
+                             choices = c("Medley")),
+  navlistPanel(
              tabPanel("Champions Series", "content"),
              tabPanel("Championships (25m)", "contents"),
              tabPanel("Championships (50m)", "contents"),
@@ -17,10 +20,71 @@ ui <- fluidPage(
              tabPanel("Olympic Games", "contents"),
              tabPanel("World Cup", "contents"),
              tabPanel("Youth Olympic Games", "contents"))))
-)
 server <- function(input, output, session) {
   competition_name <- reactive({
-    detailed_results %>% filter(series == detailed_results$series)
+    detailed_results %>% filter(series == input$Competition) %>%
+      filter(style == input$Style)
+  })
+}
+
+shinyApp(ui, server)
+
+library(tidyverse)
+
+detailed_results <- fina_join()
+detailed_results %>% group_by(series) %>% summarise(n = n())
+detailed_results %>% group_by(style) %>% summarise(n = n())
+detailed_results %>% group_by(distance) %>% summarise(n = n())
+detailed_results %>% group_by(year) %>% summarise(n = n())
+detailed_results %>% filter(year == 2020)
+library(shiny)
+
+ui <- fluidPage(sidebarLayout(
+  sidebarPanel((selectizeInput("Competition",
+                               label = "Choose the Competition",
+                               choices = c("Champions Series", "Championships (25m)", "Championships (50m)", "Junior Championships", "Marathon Series", "Olympic Games", "World Cup", "Youth Olympic Games"))
+                ),
+  (radioButtons("Style",
+                label = "Choose the Stroke",
+                choices = c("Medley", "Backstroke", "Breaststroke", "Butterfly", "Freestyle"))),
+  (sliderInput("Distance",
+               label = "Select the Distance",
+               min = 50,
+               max = 1500,
+               value = 100,
+               step = 50)),
+  (radioButtons("Relay",
+                      label = "Relay?",
+                      choices = c("TRUE", "FALSE"))),
+  (numericInput("Year",
+                label = "Select the Year",
+                min = 1924,
+                max = 2019,
+                value = 2000,
+                step = 1)),
+  (radioButtons("Gender",
+                label = "Gender",
+                choices = c("Men", "Women")))),
+  mainPanel(tabsetPanel(
+    tabPanel("Rankings", tableOutput("place_table")),
+    tabPanel("Tab 2", "contents"),
+    tabPanel("Tab 3", "contents")
+  ))
+  
+))
+server <- function(input, output, session) {
+  competition_name <- reactive({
+    detailed_results %>% filter(series == input$Competition) %>%
+      filter(style == input$Style) %>%
+      filter(distance == input$Distance) %>%
+      filter(relay == input$Relay) %>%
+      filter(year == input$Year) %>%
+      filter(gender == input$Gender)
+  })
+  output$place_table <- renderTable({
+    competition_name() %>% filter(phase_label == "Final") %>% 
+      filter(rank == 1 | rank == 2 | rank == 3) %>%
+      select(rank, ioc_code, family_name, first_name, time)
   })
 }
 
--- updates.md
+++ updates.md
@@ -9,3 +9,7 @@ I spent a lot of time trying to find a data set that will work. Once I found thi
 ## March 23 End of Class
 
 I began putting information into my R app. I worked on attempting to get the tabs up and running and am working through connecting clicking on tabs to filtering particular data, however I am not sure if this is possible so I am still working through that.
+
+## March 28 Beginning of Class
+
+In between the last two classes I worked on the best way to show my data visualizations and decided on a sort of template to follow. I created the majority of the inputs that will be used in this app. I also messed around with the templates and decided where it would be best to put the tabs and what exactly I wanted the tabs to represent. I also played around with the data to start being able to visualize the data in the app.
